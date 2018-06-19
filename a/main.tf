a7f44792e2cc0705da9dc90eaf47b8951a8c40ce
danmerron-sig <dmerron@sportingsolutions.com>
2018-06-19 12:30:27 +0100

    fixed module path

--- a/main.tf
+++ a/main.tf
@@ -1,5 +1,5 @@
 module "record" {
-  source         = "git::https://github.com/hmcts/moj-terraform-module-dns.git//module/a"
+  source         = "git::https://github.com/hmcts/moj-terraform-module-dns.git//module/A"
   name           = "${var.name}"
   zone           = "${var.zone}"
   resource_group = "${var.resource_group}"
